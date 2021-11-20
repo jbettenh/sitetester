bbb1f47cc8256f85eb75ff6cbafe62b5746cc269
Joseph Bettenhausen <jbettenh@users.noreply.github.com>
2021-11-20 20:47:57 +0100

    Test google

--- tests/test_sitemaptester.py
+++ tests/test_sitemaptester.py
@@ -1,5 +1,15 @@
 from sitemaptester import __version__
 
+import requests
+
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_page():
+    url = 'https://www.google.com'
+
+    response = requests.get(url)
+
+    assert response.status_code == 200
