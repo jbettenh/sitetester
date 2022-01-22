e287f706815760f8e414b5c2d5c828f63bd372a8
Joseph Bettenhausen <jbettenh@users.noreply.github.com>
2022-01-22 10:53:59 +0100

    Cleanup class and parametrize response time test

--- pages/homepage.py
+++ pages/homepage.py
@@ -1,13 +1,8 @@
 from bs4 import BeautifulSoup
 
-from selenium.webdriver.common.by import By
-
 
 class PageBody:
     URL = 'https://www.raspberrypi.org'
-
-    RESULT_LINKS = (By.CSS_SELECTOR, 'a.result__a')
-    SEARCH_INPUT = (By.ID, 'search_form_input')
 
     def __init__(self, browser):
         self.browser = browser
@@ -17,16 +12,6 @@ class PageBody:
 
     def title(self):
         return self.browser.title
-
-    def result_link_titles(self):
-        links = self.browser.find_elements(*self.RESULT_LINKS)
-        titles = [link.text for link in links]
-        return titles
-
-    def search_input_value(self):
-        search_input = self.browser.find_element(*self.SEARCH_INPUT)
-        value = search_input.get_attribute('value')
-        return value
 
     def get_links(self, search_class):
         soup = BeautifulSoup(self.browser.page_source, 'html.parser')
--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -6,32 +6,32 @@ from pages.homepage import PageBody
 
 
 urls = [
-    ('https://www.raspberrypi.org/', 200),
-    ('https://www.raspberrypi.org/teach', 200),
-    ('https://teachcomputing.org/', 200),
-    ('https://isaaccomputerscience.org/', 200),
-    ('https://isaaccomputerscience.org/', 200),
-    ('https://www.raspberrypi.org/research', 200),
-    ('https://helloworld.raspberrypi.org/', 200),
-    ('https://www.raspberrypi.org/learn', 200),
-    ('https://coderdojo.com/', 200),
-    ('https://codeclub.org/', 200),
-    ('https://codeclubworld.org/', 200),
-    ('https://projects.raspberrypi.org', 200),
-    ('https://astro-pi.org/', 200),
-    ('https://online.coolestprojects.org/', 200),
-    ('https://www.raspberrypi.org/safeguarding', 200),
-    ('https://www.raspberrypi.org/accessibility', 200),
-    ('https://www.raspberrypi.org/privacy', 200),
-    ('https://www.raspberrypi.org/cookies', 200),
-    ('https://www.raspberrypi.org/about', 200),
-    ('https://www.raspberrypi.org/donate', 200),
-    ('https://www.raspberrypi.org/about/meet-the-team', 200),
-    ('https://raspberrypifoundation.workable.com', 200),
-    ('https://www.raspberrypi.org/about/governance', 200),
-    ('https://www.raspberrypi.org/contact', 200),
-    ('https://www.raspberrypi.org/trademark-rules', 200),
-    ('https://www.raspberrypi.com', 200),
+    ('https://www.raspberrypi.org/', 200, 1),
+    ('https://www.raspberrypi.org/teach', 200, 1),
+    ('https://teachcomputing.org/', 200, 1),
+    ('https://isaaccomputerscience.org/', 200, 1),
+    ('https://isaaccomputerscience.org/', 200, 1),
+    ('https://www.raspberrypi.org/research', 200, 1),
+    ('https://helloworld.raspberrypi.org/', 200, 1),
+    ('https://www.raspberrypi.org/learn', 200, 1),
+    ('https://coderdojo.com/', 200, 1),
+    ('https://codeclub.org/', 200, 1),
+    ('https://codeclubworld.org/', 200, 1),
+    ('https://projects.raspberrypi.org', 200, 1),
+    ('https://astro-pi.org/', 200, 1),
+    ('https://online.coolestprojects.org/', 200, 1),
+    ('https://www.raspberrypi.org/safeguarding', 200, 1),
+    ('https://www.raspberrypi.org/accessibility', 200, 1),
+    ('https://www.raspberrypi.org/privacy', 200, 1),
+    ('https://www.raspberrypi.org/cookies', 200, 1),
+    ('https://www.raspberrypi.org/about', 200, 1),
+    ('https://www.raspberrypi.org/donate', 200, 1),
+    ('https://www.raspberrypi.org/about/meet-the-team', 200, 1),
+    ('https://raspberrypifoundation.workable.com', 200, 1),
+    ('https://www.raspberrypi.org/about/governance', 200, 1),
+    ('https://www.raspberrypi.org/contact', 200, 1),
+    ('https://www.raspberrypi.org/trademark-rules', 200, 1),
+    ('https://www.raspberrypi.com', 200, 1),
 ]
 
 
@@ -39,18 +39,19 @@ def test_version():
     assert __version__ == '0.1.0'
 
 
-@pytest.mark.parametrize('url, expected_code', urls)
-def test_page_exists(url, expected_code):
+@pytest.mark.parametrize('url, expected_code, elapsed', urls)
+def test_page_exists(url, expected_code, elapsed):
     """ Check all pages are available """
     response = requests.get(url)
 
     assert response.status_code == expected_code
 
 
-def test_response_time():
-    response = requests.get('https://www.raspberrypi.org/')
+@pytest.mark.parametrize('url, expected_code, elapsed', urls)
+def test_response_time(url, expected_code, elapsed):
+    response = requests.get(url)
 
-    assert response.elapsed.total_seconds() < 1.00
+    assert response.elapsed.total_seconds() < elapsed
 
 
 def test_number_of_pages():
@@ -59,18 +60,21 @@ def test_number_of_pages():
 
 def test_html_headers(browser):
     title = 'Teach, Learn, and Make with Raspberry Pi'
-    phrase = 'https://teachcomputing.org/'
+
     page = PageBody(browser)
     page.load()
 
-    titles = page.get_links('c-footer__nav-link')
-    print(titles)
-    assert len(titles) > 0
     assert title == page.title()
 
 
 def test_html_elements(browser):
-    pass
+    phrase = 'https://teachcomputing.org/'
+    page = PageBody(browser)
+
+    page.load()
+    links = page.get_links('c-footer__nav-link')
+
+    assert len(links) > 0
 
 
 def test_json_response():
